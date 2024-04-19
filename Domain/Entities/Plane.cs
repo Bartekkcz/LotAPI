9b2d0b79892765012e4745c5270820c492ff03c7
bartekkcz <kaczmarczyk.bartosz@outlook.com>
2024-04-19 22:13:02 +0200

    Implement repository and domain classes. Add referencess

---
+++ Domain/Entities/Plane.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Plane : AuditableEntity
+    {
+        public int Id { get; set; }
+        public string FlightNumber { get; set; }
+        public DateTime? DepartureDate { get; set; }
+        public string DeparturePlace { get; set; }
+        public string ArrivalPlace { get; set; }
+        public string PlaneType { get; set; }
+
+        public Plane() { }
+
+        public Plane(int id, string flightNumber, DateTime? departureDate, string departurePlace, string arrivalPlace, string planeType)
+        {
+            (Id, FlightNumber, DepartureDate, DeparturePlace, ArrivalPlace, PlaneType) = (id, flightNumber, departureDate, departurePlace, arrivalPlace, planeType);
+        }
+    }
+}
---
+++ Infrastructure/Repositories/PlaneRepository.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class PlaneRepository : IPlaneRepository
+    {
+        private static readonly ISet<Domain.Entities.Plane> _planes = new HashSet<Domain.Entities.Plane>()
+        {
+            new Domain.Entities.Plane(1, "LO123", DateTime.UtcNow, "Warszawa", "Berlin", "Boeing 787 Dreamliner"),
+            new Domain.Entities.Plane(2, "LO456", DateTime.UtcNow, "Kraków", "Amsterdam", "Embraer 195"),
+            new Domain.Entities.Plane(3, "LO789", DateTime.UtcNow, "Gdańsk", "Londyn", "Boeing 737-800")
+        };
+
+        public IEnumerable<Domain.Entities.Plane> GetALL()
+        {
+            return _planes;
+        }
+
+        public Domain.Entities.Plane GetById(int id)
+        {
+            return _planes.SingleOrDefault(x => x.Id == id);
+        }
+
+        public Domain.Entities.Plane Add(Domain.Entities.Plane plane)
+        {
+            plane.Id = _planes.Count() + 1;
+            plane.Created = DateTime.UtcNow;
+            _planes.Add(plane);
+            return plane;
+        }
+
+        public void Update(Domain.Entities.Plane plane)
+        {
+            plane.LastModified = DateTime.UtcNow;
+        }
+
+        public void Delete(Domain.Entities.Plane plane)
+        {
+            _planes.Remove(plane);
+        }
+    }
+}
